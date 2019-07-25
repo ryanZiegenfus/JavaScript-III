9244f7f9a4ea70df0a9dfb7c439156826bc06bfd
Ryan Ziegenfus <thewebcoderz@gmail.com>
2019-07-25 03:32:51 -0400

    completed mvp goals for prototypes.js and updated this.js

--- assignments/this.js
+++ assignments/this.js
@@ -1,9 +1,9 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
+* 1. Window - When 'this' isnt used inside of an object, it binds to the window (which is the first object it sees in its scope)
+* 2. implicit - This is when 'this' is used inside of an object literal. 'this' then binds to the object it's inside of.
+* 3. New binding - This binds 'this' to the object returned by the constructor. In other words, when you use new to create a new object, 'this' binds to that new object.
 * 4. 
 *
 * write out a code example of each explanation above
